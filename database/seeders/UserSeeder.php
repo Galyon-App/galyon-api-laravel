1fe2ff1fd9f30f6ad2c89e7d21cca4ce18dc089f
Bytes Crafter <bytescrafter@gmail.com>
2021-05-05 10:51:42 +0800

    Added factory with its seeder on on user table with relationship to meta table.

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use App\Models\UserMeta;
+use Illuminate\Database\Seeder;
+use Faker\Generator;
+use Illuminate\Container\Container;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * The current Faker instance.
+     *
+     * @var \Faker\Generator
+     */
+    protected $faker;
+
+    /**
+     * Create a new seeder instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->faker = $this->withFaker();
+    }
+
+    /**
+     * Get a new Faker instance.
+     *
+     * @return \Faker\Generator
+     */
+    protected function withFaker()
+    {
+        return Container::getInstance()->make(Generator::class);
+    }
+
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        User::factory()
+            ->count(10)
+            ->create()
+            ->each(function($user) {
+                UserMeta::create(array(
+                    'uuid'=>$user->uuid,
+                    'meta_key'=>'uname',
+                    'meta_val'=> $this->faker->unique()->userName()
+                ));
+
+                UserMeta::create(array(
+                    'uuid'=>$user->uuid,
+                    'meta_key'=>'phone',
+                    'meta_val'=> $this->faker->unique()->phoneNumber()
+                ));
+
+                UserMeta::create(array(
+                    'uuid'=>$user->uuid,
+                    'meta_key'=>'email',
+                    'meta_val'=> $this->faker->unique()->safeEmail()
+                ));
+            });
+    }
+}
