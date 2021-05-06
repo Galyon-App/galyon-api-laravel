cc0c6cff86abcd7f1363eee5abe3445a5ebe55c9
Bytes Crafter <bytescrafter@gmail.com>
2021-05-07 00:19:36 +0800

    Added migration for products table.

---
+++ Modules/ECommerce/Database/Migrations/2021_05_06_161737_create_products_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateProductsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('uuid', 36)
+                ->index('uuid', 'products_uuid_index');
+            $table->string('title', 255);
+            $table->mediumText('desc')
+                ->default('');
+            $table->boolean('is_active');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+}
