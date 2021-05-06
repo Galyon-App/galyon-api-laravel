0e51fd64f818ed3a7407803d3eced4782e46e324
Bytes Crafter <bytescrafter@gmail.com>
2021-05-06 23:40:53 +0800

    Added users accesses, roles, and permissions migrations.

---
+++ database/migrations/2021_05_06_152242_create_users_permissions_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateUsersPermissionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('users_permissions', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('role_uuid', 36);
+            $table->string('permit_key', 255);
+            $table->string('permit_val', 255);
+            $table->string('module', 255);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('users_permissions');
+    }
+}
