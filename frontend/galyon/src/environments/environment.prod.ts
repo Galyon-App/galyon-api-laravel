0fe95809507ba2fc9ef18e9390557255e96a2934
Bytes Crafter <bytescrafter@gmail.com>
2021-05-07 19:02:37 +0800

    Added all packages that we might need on the development and environment config for firebase.

--- frontend/galyon/src/environments/environment.prod.ts
+++ frontend/galyon/src/environments/environment.prod.ts
@@ -1,4 +1,13 @@
 export const environment = {
   production: true,
-  baseURL: 'https://api.galyon.app/'
+  baseURL: 'https://api.galyon.app/',
+  firebase: {
+    apiKey: "AIzaSyDBy1btu2_RUN2r4tWTtUY2Zhw4oJsum6E",
+    authDomain: "businext-app.firebaseapp.com",
+    projectId: "businext-app",
+    storageBucket: "businext-app.appspot.com",
+    messagingSenderId: "71633264934",
+    appId: "1:71633264934:web:97e024337f1a70b4e6e1e7",
+    measurementId: "G-X4D1D5TXDX"
+  }
 };
--- frontend/galyon/src/environments/environment.ts
+++ frontend/galyon/src/environments/environment.ts
@@ -4,7 +4,16 @@
 
 export const environment = {
   production: false,
-  baseURL: 'http://localhost:8000'
+  baseURL: 'http://localhost:8000',
+  firebase: {
+    apiKey: "AIzaSyDBy1btu2_RUN2r4tWTtUY2Zhw4oJsum6E",
+    authDomain: "businext-app.firebaseapp.com",
+    projectId: "businext-app",
+    storageBucket: "businext-app.appspot.com",
+    messagingSenderId: "71633264934",
+    appId: "1:71633264934:web:97e024337f1a70b4e6e1e7",
+    measurementId: "G-X4D1D5TXDX"
+  }
 };
 
 /*
