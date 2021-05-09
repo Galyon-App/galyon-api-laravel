e39520ab08ef109a21451e014b0a1ed8cc089107
Bytes Crafter <bytescrafter@gmail.com>
2021-05-10 01:13:41 +0800

    Added store metas migration file on Tindero module.

---
+++ Modules/Tindero/Database/Migrations/2021_05_09_171105_create_stores_metas_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateStoresMetasTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('stores_metas', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('stoe_uuid', 36);
+            $table->string('meta_key', 255);
+            $table->longText('meta_val');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('stores_metas');
+    }
+}
