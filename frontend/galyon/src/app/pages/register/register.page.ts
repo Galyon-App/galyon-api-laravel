da94f25bf5580cb010476ad1b802e8d35a09fdc9
Bytes Crafter <bytescrafter@gmail.com>
2021-05-15 02:38:45 +0800

    Added and stylize a multi theme register page on frontend galyon app.

---
+++ frontend/galyon/src/app/pages/register/register.page.ts
@@ -0,0 +1,91 @@
+import { Component } from '@angular/core';
+import { NavController } from '@ionic/angular';
+import { UtilService } from 'src/app/services/util.service';
+import { Router } from '@angular/router';
+import * as moment from 'moment';
+import { ApiService } from 'src/app/services/api.service';
+import { InAppBrowser } from '@ionic-native/in-app-browser/ngx';
+import { LogService } from 'src/app/services/log.service';
+
+@Component({
+  selector: 'app-register',
+  templateUrl: './register.page.html',
+  styleUrls: ['./register.page.scss'],
+})
+export class RegisterPage {
+
+  fname: any = '';
+  lname: any = '';
+  gender: any = '1';
+  email: any = '';
+  password: any = '';
+
+  isBusy: boolean;
+  check: boolean;
+
+  dummy: any[] = [];
+  constructor(
+    private navCtrl: NavController,
+    public util: UtilService,
+    private router: Router,
+    private api: ApiService,
+    private iab: InAppBrowser,
+    public log: LogService,
+  ) {
+    this.dummy = this.util.countrys;
+  }
+
+  register() {
+
+    if (!this.check) {
+      this.util.showToast(this.util.getString('Please accept terms and conditions'), 'dark', 'bottom');
+      return false;
+    }
+
+    const emailfilter = /^[\w._-]+[+]?[\w._-]+@[\w.-]+\.[a-zA-Z]{2,6}$/;
+    if (!emailfilter.test(this.email)) {
+      this.util.showToast(this.util.getString('Please enter valid email'), 'dark', 'bottom');
+      return false;
+    }
+
+    if (!this.fname || !this.lname || !this.email || !this.password) {
+      this.util.showToast(this.util.getString('All Fields are required'), 'dark', 'bottom');
+      return false;
+    }
+
+    const param = {
+      fname: this.fname,
+      lname: this.lname,
+      email: this.email,
+      password: this.password,
+      password_confirmation: this.password,
+      gender: this.gender,
+    };
+
+    this.isBusy = true;
+    this.api.post('api/auth/register', param).subscribe((response: any) => {
+      this.isBusy = false;
+      console.log(response);
+
+      if (response && response.success) {
+        localStorage.setItem(this.util.getPrefix('uuid'), response.uuid);
+        localStorage.setItem(this.util.getPrefix('token'), response.token);
+        this.navCtrl.navigateRoot(['home']);
+      } else {
+        this.util.errorToast(response.data.message);
+      }
+    }, error => {
+      this.isBusy = false;
+      this.log.error(error);
+      this.util.errorToast(this.util.getString('Something went wrong'));
+    });
+  }
+
+  back() {
+    this.router.navigate(['/login']);
+  }
+
+  privacy() {
+    this.iab.create('http://galyon.app/privacy');
+  }
+}
