3cd0040b59c695b5faae83c1417796035748c970
Bytes Crafter <bytescrafter@gmail.com>
2021-05-15 18:37:34 +0800

    Added blank or initial layout for account tab page on frontend galyon.

--- frontend/galyon/src/app/pages/account/account.page.ts
+++ frontend/galyon/src/app/pages/account/account.page.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ApiService } from 'src/app/services/api.service';
+import { UtilService } from 'src/app/services/util.service';
+import { LogService } from 'src/app/services/log.service';
 
 @Component({
   selector: 'app-account',
@@ -7,9 +10,64 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AccountPage implements OnInit {
 
-  constructor() { }
+  constructor(
+    private api: ApiService,
+    public util: UtilService,
+    public log: LogService,
+  ) { }
 
   ngOnInit() {
   }
 
+  getProfile() {
+    return null;
+  }
+
+  getName() {
+    return 'Juan Dela Cruz';
+  }
+
+  getEmail() {
+    return 'juandelacruz@bytescrafter.net';
+  }
+
+  editProfile() {
+    //Do something
+  }
+
+  myAddress() {
+    //Do something
+  }
+
+  myOrder() {
+    //Do something
+  }
+
+  myFavorite() {
+    //Do something
+  }
+
+  appContact() {
+    //Do something
+  }
+
+  appAbout() {
+    //Do something
+  }
+
+  appFaqs() {
+    //Do something
+  }
+
+  appHelp() {
+    //Do something
+  }
+
+  appSupport() {
+    //Do something
+  }
+
+  logout() {
+    //Do something
+  }
 }
